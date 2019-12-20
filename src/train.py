cff14fdb817397dbd16d55424fe7f2f1f1f06e74
semiwaker <semiwaker@163.com>
2019-12-20 20:38:57 +0800

    use tf.dataset

--- src/train.py
+++ src/train.py
@@ -54,6 +54,27 @@ class AccumulatedLoss:
         return f"{name} loss: G {self.G_loss/cnt} D {self.D_loss/cnt}"
 
 
+def calc_loss(x, y, G, D):
+    z = G(x)
+    d_logits = D(inputs=(y, x))
+    d2_logits = D(inputs=(z, x))
+
+    d_loss = entropy_loss(d_logits, 1) + entropy_loss(d2_logits, 0)
+    g_loss = entropy_loss(d2_logits, 1) + config.LAMBDA * L1_loss(y, z)
+
+    return g_loss, d_loss
+
+
+def test(dataset, G, D):
+    g_sum = 0.0
+    d_sum = 0.0
+    for (x, y) in enumerate(dataset):
+        g_loss, d_loss = calc_loss(x, y, G, D)
+        g_sum += g_loss
+        d_sum += d_loss
+    return g_sum / len(dataset), d_sum / len(dataset)
+
+
 def train(dataset_name, verbose, make_graph):
     _verbose = verbose
     EPOCHS = config.EPOCHS
@@ -62,7 +83,8 @@ def train(dataset_name, verbose, make_graph):
     BETA2 = config.ADAM_BETA2
 
     # Preparing
-    dataset = data.Dataset([dataset_name])
+    train_dataset = data.make_dataset(dataset_name, "train")
+    test_dataset = data.make_dataset(dataset_name, "test")
 
     G = generator()
     D = discriminator()
@@ -73,8 +95,10 @@ def train(dataset_name, verbose, make_graph):
     # set up pyplot
     if make_graph:
         plt.ion()
-        g_loss = []
-        d_loss = []
+        g_loss_train = []
+        g_loss_test = []
+        d_loss_train = []
+        d_loss_test = []
         batch_cnt = []
         total_batch = 0
         plt.legend("best")
@@ -82,7 +106,8 @@ def train(dataset_name, verbose, make_graph):
     # Start training
     VPrint("Start training")
     global_timer = Timer()
-    global_loss = AccumulatedLoss("Global")
+    global_train_loss = AccumulatedLoss("Global train")
+    global_test_loss = AccumulatedLoss("Global test")
 
     G.train()
     D.train()
@@ -90,19 +115,15 @@ def train(dataset_name, verbose, make_graph):
     for epoch in range(1, EPOCHS+1):
         epoch_timer = Timer()
         VPrint(f"Start epoch {epoch}")
-        epoch_loss = AccumulatedLoss("Epoch")
+        epoch_train_loss = AccumulatedLoss("Epoch train")
+        epoch_test_loss = AccumulatedLoss("Epoch test")
         running_loss = AccumulatedLoss("Running")
 
-        for (x, y), i in enumerate(dataset):
+        for (x, y), i in enumerate(train_dataset):
             total_batch += 1
 
             with tf.GradientTape(persistent=True) as tape:
-                z = G(x)
-                d_logits = D(inputs=(y, x))
-                d2_logits = D(inputs=(z, x))
-
-                d_loss = entropy_loss(d_logits, 1) + entropy_loss(d2_logits, 0)
-                g_loss = entropy_loss(d2_logits, 1) + config.LAMBDA * L1_loss(y, z)
+                g_loss, d_loss = calc_loss(x, y, G, D)
 
             # train generator
             grad = tape.gradient(g_loss, G.trainable_weights)
@@ -114,36 +135,56 @@ def train(dataset_name, verbose, make_graph):
             # Caculate batch loss
             batch_D_loss = np.average(d_loss.to_numpy())
             batch_G_loss = np.average(g_loss.to_numpy())
-            global_loss.accumulate(batch_G_loss, batch_D_loss)
-            epoch_loss.accumulate(batch_G_loss, batch_D_loss)
+            global_train_loss.accumulate(batch_G_loss, batch_D_loss)
+            epoch_train_loss.accumulate(batch_G_loss, batch_D_loss)
             running_loss.accumulate(batch_G_loss, batch_D_loss)
 
             if i % config.OUTPUT_FREQ == 0:
                 VPrint(running_loss, f"time used:{epoch_timer()}")
                 running_loss.reset()
 
+                test_g_loss, test_d_loss = test(test_dataset, G, D)
+                global_test_loss.accumulate(test_g_loss, test_d_loss)
+                epoch_test_loss.accumulate(test_g_loss, test_d_loss)
+
                 # make graph
-                g_loss.append(batch_G_loss)
-                d_loss.append(batch_D_loss)
+                g_loss_train.append(batch_G_loss)
+                d_loss_train.append(batch_D_loss)
+                g_loss_test.append(test_g_loss)
+                d_loss_test.append(test_d_loss)
                 batch_cnt.append(total_batch)
                 if make_graph:
-                    plt.plot(batch_cnt, g_loss, label="generator loss")
-                    plt.plot(batch_cnt, d_loss, label="discriminator loss")
+                    plt.plot(batch_cnt, g_loss_train,
+                             label="generator train loss")
+                    plt.plot(batch_cnt, d_loss_train,
+                             label="discriminator train loss")
+                    plt.plot(batch_cnt, g_loss_test,
+                             label="generator test loss")
+                    plt.plot(batch_cnt, d_loss_test,
+                             label="discriminator test loss")
                     plt.draw()
 
         VPrint(f"Epoch {epoch} time used:{epoch_timer()}")
-        VPrint(epoch_loss)
+        VPrint(epoch_train_loss)
+        VPrint(epoch_test_loss)
 
         # make graph
         if make_graph:
             plt.ioff()
-            plt.plot(batch_cnt, g_loss, label="generator loss")
-            plt.plot(batch_cnt, d_loss, label="discriminator loss")
+            plt.plot(batch_cnt, g_loss_train,
+                     label="generator train loss")
+            plt.plot(batch_cnt, d_loss_train,
+                     label="discriminator train loss")
+            plt.plot(batch_cnt, g_loss_test,
+                     label="generator test loss")
+            plt.plot(batch_cnt, d_loss_test,
+                     label="discriminator test loss")
             plt.savefig(config.LOSS_PLOT_PATH, dpi=120, quality=100)
             plt.show()
 
     VPrint(f"End training. Time used {global_timer()}")
-    VPrint(global_loss)
+    VPrint(global_train_loss)
+    VPrint(global_test_loss)
 
     # save model
     G.save_weights(config.G_SAVE_PATH+'_'+dataset_name)
